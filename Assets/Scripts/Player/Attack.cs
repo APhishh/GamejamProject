5d78aa9fb0e22f9a5a5fe4e0557d9a329b2adab5
Choconut06 <96691604+elderaka@users.noreply.github.com>
2025-05-11 12:49:52 +0700

    Update/Added Controller Support
    TODO: Fix animator lol

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -5,39 +5,38 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-
     [SerializeField] GameObject hitbox;
     [SerializeField] GameObject hitParticle;
-    [SerializeField] Animator animator;
-    [SerializeField]private bool attacking;
+    [SerializeField] Animator animator; // Animator remains, but we won't use it
+    [SerializeField] private bool attacking;
     [SerializeField] private float swingDelay;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0) && attacking == false)
+        if (Input.GetMouseButtonDown(0) && attacking == false)
         {
             attacking = true;
-            animator.SetBool("Attacking", attacking);
+            // animator.SetBool("Attacking", attacking); // Disabled for debugging
             StartCoroutine(Attacking());
         }
-        animator.SetBool("Attacking", attacking);
+        // animator.SetBool("Attacking", attacking); // Disabled for debugging
     }
 
     IEnumerator Attacking()
     {
-            yield return new WaitForSeconds(0.3f);
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.z = 0f;
-            Vector3 dir = (mousePos - transform.position).normalized;
-            float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
-            GameObject newObj = Instantiate(hitbox, transform.position + new Vector3(dir.x*1,dir.y*1), Quaternion.Euler(0, 0, angle));
-            HitboxFollowCharacter followScript = newObj.GetComponent<HitboxFollowCharacter>();
-            CollisionDetection colScript = newObj.GetComponent<CollisionDetection>();
-            Animator hitboxAnimator = newObj.GetComponent<Animator>();
-            colScript.setAttacker(gameObject);
-            followScript.Set(transform,dir);
-            StartCoroutine(Despawn(newObj));
+        yield return new WaitForSeconds(0.3f);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        Vector3 dir = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        GameObject newObj = Instantiate(hitbox, transform.position + new Vector3(dir.x * 1, dir.y * 1), Quaternion.Euler(0, 0, angle));
+        HitboxFollowCharacter followScript = newObj.GetComponent<HitboxFollowCharacter>();
+        CollisionDetection colScript = newObj.GetComponent<CollisionDetection>();
+        Animator hitboxAnimator = newObj.GetComponent<Animator>();
+        colScript.setAttacker(gameObject);
+        followScript.Set(transform, dir);
+        StartCoroutine(Despawn(newObj));
     }
 
     IEnumerator Despawn(GameObject obj)
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,33 +10,48 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float movementSpeed;
     [SerializeField] private float jumpPower;
     [SerializeField] private float jumpCooldown;
-    [SerializeField] Animator animator;
+    [SerializeField] Animator animator; // Animator remains, but we won't use it
     [SerializeField] private SpriteRenderer SR;
     private bool canJump = true;
-    // Update is called once per frame
+
     void Update()
     {
+        HandleMovement();
+        HandleJump();
+    }
 
-        animator.SetFloat("Speed", math.abs(playerRB.velocity.x));
-        if (Input.GetKeyDown(KeyCode.Space) && canJump == true)
-        {
-            canJump = false;
-            playerRB.velocity= new Vector3(playerRB.velocity.x, jumpPower);
-            StartCoroutine(JumpCooldown());
-        }
-
+    void HandleMovement()
+    {
+        // Get input from both keyboard and controller horizontal axis
         float velX = Input.GetAxis("Horizontal") * movementSpeed;
 
-        if(velX < 0)
+        // Also support D-pad input
+        velX += Input.GetAxis("DPad_Horizontal") * movementSpeed;
+
+        // animator.SetFloat("Speed", math.abs(playerRB.velocity.x)); // Disabled for debugging
+
+        // Update sprite direction
+        if (velX < 0)
         {
             SR.flipX = true;
         }
-        else if(velX > 0)
+        else if (velX > 0)
         {
             SR.flipX = false;
         }
 
-        playerRB.velocity = new Vector2(velX,playerRB.velocity.y); 
+        playerRB.velocity = new Vector2(velX, playerRB.velocity.y);
+    }
+
+    void HandleJump()
+    {
+        // Support both keyboard space and controller button (usually A/X button)
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Jump")) && canJump)
+        {
+            canJump = false;
+            playerRB.velocity = new Vector3(playerRB.velocity.x, jumpPower);
+            StartCoroutine(JumpCooldown());
+        }
     }
 
     IEnumerator JumpCooldown()
